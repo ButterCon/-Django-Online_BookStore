46ac1cc9d1b4e7bd625c5b858c082d400c09ca41
진현우 <49802982+jhw3979@users.noreply.github.com>
2020-11-03 10:33:18 +0900

    Add page (구매완료)
    구매완료 페이지 추가

---
+++ DB_project(20.11.03)/bookstore/urls.py
@@ -0,0 +1,20 @@
+from django.urls import path
+from . import views
+
+app_name = "bookstore"
+
+urlpatterns = [
+    path('<str:name>/home/', views.homePage, name='home'),
+    path('<str:name>/<int:book_id>/sinorder/', views.sinOrderPage, name='sinorder'),
+    path('<str:name>/mulorder/', views.mulOrderPage, name='mulorder'),
+    path('<str:name>/orderdone/', views.orderdonePage, name='orderdone'),
+    path('<str:name>/cart/', views.cartPage, name='cart'),
+    path('<str:name>/<int:book_id>/cartadd/', views.cartaddPage, name='cartadd'),
+    path('<str:name>/<int:book_id>/cartdel/', views.cartdelPage, name='cartdel'),
+    path('reg/', views.regPage, name='reg'),
+    path('regCon/', views.regConPage, name='regCon'),
+    path('<str:name>/userinfo/', views.userinfoPage, name='userinfo'),
+    path('<str:name>/sdadd/', views.sdaddPage, name='sdadd'),
+    path('<str:name>/cardadd/', views.cardaddPage, name='cardadd'),
+    path('login/', views.loginPage, name='login'),
+]

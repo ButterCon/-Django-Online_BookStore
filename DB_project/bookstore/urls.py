ffa515ad8fa8033acda8acfc7d2f377199935838
진현우 <49802982+jhw3979@users.noreply.github.com>
2020-11-09 16:55:53 +0900

    cartdel수정해야됨

--- DB_project/bookstore/urls.py
+++ DB_project/bookstore/urls.py
@@ -4,20 +4,24 @@ from . import views
 app_name = "bookstore"
 
 urlpatterns = [
+    path('login/', views.loginPage, name='login'),
     path('<str:name>/home/', views.homePage, name='home'),
-    path('<str:name>/<int:BookOrder_id>/order/', views.orderPage, name='order'),
+    ############################주문####################################
+    path('<str:name>/order/', views.orderPage, name='order'),
     path('<str:name>/<int:book_id>/order/', views.orderConPage, name='ordercon'),
     path('<str:name>/orderdone/', views.orderdonePage, name='orderdone'),
+    ############################장바구니#################################
     path('<str:name>/cart/', views.cartPage, name='cart'),
     path('<str:name>/<int:book_id>/cart/', views.cartaddPage, name='cartadd'),
-    path('<str:name>/<int:Order_id>/<int:book_id>/cart/', views.cartdelPage, name='cartdel'),
+    path('<str:name>/<int:book_id>/cart/', views.cartdelPage, name='cartdel'),
+    ############################회원가입#################################
     path('reg/', views.regPage, name='reg'),
     path('regCon/', views.regConPage, name='regCon'),
+    ############################회원정보수정##############################
     path('<str:name>/userinfo/', views.userinfoPage, name='userinfo'),
     path('<str:name>/sdadd/', views.sdaddPage, name='sdadd'),
     path('<str:name>/cardadd/', views.cardaddPage, name='cardadd'),
+    ############################쿠폰####################################
     path('<str:name>/coupon/', views.couponPage, name='coupon'),    #회원수정->쿠폰
     path('<int:BookOrder_id>/couponselect/', views.couponselectPage, name='couponselect'),    #주문->쿠폰
-    #path('<str:name>/<int:BookOrder_id>/<int:BookSB_id>/couponselectCon/', views.couponselectConPage, name='couponselectCon'),    #주문->쿠폰
-    path('login/', views.loginPage, name='login'),
 ]
--- DB_project/bookstore/views.py
+++ DB_project/bookstore/views.py
@@ -42,10 +42,9 @@ def homePage(request, name):
 
 def cartPage(request, name):
     User_qs = User.objects.get(User_name=name)
-    #Order_qs id로 넣어야됨
-    Order_qs = Order.objects.filter(User=User_qs).last()    #마지막 장바구니
-    BookOrder_qs = BookOrder.objects.filter(Order=Order_qs)
-    context = {'User': User_qs, 'Order_list': Order_qs, 'BookOrder_list': BookOrder_qs}
+    SB_qs = ShoppingBasket.objects.get(User=User_qs)
+    BookSB_qs = BookSB.objects.filter(ShoppingBasket=SB_qs)
+    context = {'User': User_qs, 'BookSB_list': BookSB_qs}
     return render(request, 'bookstore/cartPage.html', context)
 
 
@@ -63,70 +62,25 @@ def cartaddPage(request, name, book_id):
     #장바구니에 책 넣기
     BookSB(Book=Book_qs, ShoppingBasket=SB_qs).save()
     BookSB_qs = BookSB.objects.filter(ShoppingBasket=SB_qs) #장바구니 가져오기
-    #BookOrder 책 개수 저장하기
-    Book_stock = len(BookSB.objects.filter(Book=Book_qs))   #넘어온 책이 저장된 개수를 저장
-    #BookOrder 책 가격 넣기
-    Book_price_add = 0
-    for i in BookSB.objects.filter(Book=Book_qs):   #넘어온 책이 저장된 가격를 저장
-        Book_price_add += i.Book.Book_price
-    #Order -> 주문 총액 저장
-    order_totalAmount = 0
-    for i in BookSB_qs:
-        order_totalAmount += i.Book.Book_price
-    #Order 쿼리 저장
-    if len(BookSB.objects.filter(ShoppingBasket=SB_qs))==1:   #처음 저장할 때, 주문을 만들어준다, 주문 진행 할 때는 이걸로 계속 진행함
-        Order_qs = Order(User=User_qs, Order_date=datetime.datetime.now(), Order_totalprice=order_totalAmount)
-        Order_qs.save()
-    else:   #주문리스트에 저장돼 있을경우 데이터 업데이트해준다.
-        Order_qs = Order.objects.filter(User=User_qs).last()    #최신 주문가져오기
-        Order_qs.Order_totalprice = order_totalAmount   #금액 업데이트해준다, 혹시 나중에 금액 빼거나 추가할 때 업데이트 or 추가 중에 뭐가 더 좋을지 선택
-    Order_qs.save()
-    order_id = Order_qs.id  #주문번호
-    #BookOrder 쿼리 저장
-    if Book_stock == 1: #책주문리스트에 처음 책을 저장할 때, 책의개수가 1일 때, 새로운 쿼리를 생성해준다.
-        BookOrder_qs = BookOrder(Book=Book_qs, Order=Order_qs, BO_count=Book_stock, BO_price=Book_price_add)
-        BookOrder_qs.save()
-    else:   #책주문리스트에 기존에 책이 있을경우, 책 수량 +1, 책 가격 +
-        BookOrder_qs = BookOrder.objects.filter(Book=Book_qs)   #책 쿼리 찾아서 데이터 넣어줘야됨
-        BookOrder_qs.BO_count += 1  #책 수량 추가
-        BookOrder_qs.BO_price += Book_qs.Book_price #책 가격을 한번더 더해준다.
-        BookOrder_qs.save() #쿼리 저장
-    #####################쿠폰 어떡할거야?#######################
-    #models에 BO_DC_price를 전체 할인가로 할것인가, 한개의 할인가로 할것인가
-    #orderPage.html 19번줄 수정해줘야됨
-    #리턴
-    BookOrder_qs = BookOrder.objects.filter(Order=Order.objects.get(id=order_id))   #주문번호에 해당하는 책주문리스트 가져온다.
-    context = {'User': User_qs, 'BookOrder_list': BookOrder_qs} #책주문리스트에 저장된값들: 책 수량, 책 가격
+    context = {'User': User_qs, 'BookSB_list': BookSB_qs}
     return render(request, 'bookstore/cartPage.html', context)
 
 
-def cartdelPage(request, name,Order_id ,book_id):
+def cartdelPage(request, name, book_id):
     #장바구니 페이지에서 장바구니 지우기 클릭시 이벤트
     #책 재고량 +1
     #User 장바구니에 Book 제거
     User_qs = User.objects.get(User_name=name)
     Book_qs = Book.objects.get(id=book_id)  #지워야하는 책 쿼리 가져옴
     SB_qs = ShoppingBasket.objects.get(User=User_qs)
-    Order_qs = Order.objects.get(id=Order_id)   #현재 진행중인 카트id가져오기
     # 책 stock +1 해주기
     Book_qs.Book_stock += 1
     Book_qs.save()
     #유저장바구니리스트(BookSB)에 책 빼주기 -1
     BookSB_qs = BookSB.objects.filter(ShoppingBasket=SB_qs, Book=Book_qs) #장바구니에서 지워줄 책리스트 가져오기
-    BookSB_qs.first().delect()  #장바구니에 책 지워줌
-    #책주문내역리스트(BookOrder)에 책 빼주기 -1
-    #BookOrder에 책가격 뺴주기
-    BookOrder_qs = BookOrder.objects.filter(Order=Order_qs, Book=Book_qs) #책주문내역에 가져옴 책
-    if BookOrder_qs.BO_count == 1:  #책이 하나만 들어있을 경우
-        BookOrder_qs.delete()
-    elif BookOrder_qs.BO_count > 1: #책이 하나이상 들어있을 경우
-        BookOrder_qs.BO_count += -1
-        BookOrder_qs.BO_price += -Book_qs.Book_price    #책가격 뺴주기
-        Order_qs.Order_totalprice += -Book_qs.Book_price
-        Order_qs.save()
-        BookOrder_qs.save()
+    BookSB_qs.first().delete()  #장바구니에 책 지워줌
     #유저장바구니리스트에 책,장바구니 지우기
-    context = {'User': User_qs, "BookOrder_list": BookOrder_qs}
+    context = {'User': User_qs, "BookSB_list": BookSB_qs}
     return render(request, 'bookstore/cartPage.html', context)
 
 def orderConPage(request, name, book_id):   #주문 페이지 들어가기전에 쿼리 생성 및 저장
